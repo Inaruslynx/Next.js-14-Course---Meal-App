16d871d1a680f8fcb52d3fbf5b662bcbb960b422
inaruslynx <31197966+Inaruslynx@users.noreply.github.com>
2024-01-27 15:22:36 +0000

    feat: add to b/meals.db and add new file b/public/images/oatmeal.jpg

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,13 @@
+"use client";
+
 import classes from "./page.module.css";
 import { ShareMeal } from "@/lib/action";
 import ImagePicker from "@/app/components/meals/image-picker";
+import MealsFormSubmit from "@/app/components/meals/meals-form-submit";
+import { useFormState } from "react-dom";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(ShareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +17,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={ShareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -41,8 +46,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker name="image" label="Upload an image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
